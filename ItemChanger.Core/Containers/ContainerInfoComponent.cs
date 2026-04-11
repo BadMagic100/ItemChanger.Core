87be068d060b5dc806421e5a31c472b22fbbacae
Sean Dempsey <dempsey.sean@outlook.com>
2026-04-11 00:03:43 -0700

    Add docs for the now-public containerinfo

--- ItemChanger.Core/Containers/ContainerInfoComponent.cs
+++ ItemChanger.Core/Containers/ContainerInfoComponent.cs
@@ -2,7 +2,16 @@
 
 namespace ItemChanger.Containers;
 
+/// <summary>
+/// Component that is attached to GameObjects by Containers in <see cref="Container.GetNewContainer(ContainerInfo)"/>
+/// or <see cref="Container.ModifyContainerInPlace(GameObject, ContainerInfo)"/> in order to attach container information
+/// to modified objects.
+/// </summary>
+/// <seealso cref="ContainerInfo.FindContainerInfo(GameObject)"/>
 public sealed class ContainerInfoComponent : MonoBehaviour
 {
+    /// <summary>
+    /// The ContainerInfo to attach
+    /// </summary>
     public required ContainerInfo Info { get; init; }
 }
