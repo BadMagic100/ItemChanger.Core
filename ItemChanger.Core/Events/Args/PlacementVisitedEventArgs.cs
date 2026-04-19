f9fad386070bcf05dd264f711f98e0c955ee7189
Sean Dempsey <dempsey.sean@outlook.com>
2026-04-18 20:52:43 -0700

    feat!: align OnVisited to name and docs to behavior
    Remove misleading documentation and naming that suggests OnVisitStateChanged is only invoked on changes. Various improvements in the ability to detect changes, including replacing NoChange with HasChangedAny and HasChangedFlags helpers
    
    UpdatedSetBoolOnGiveTag to use the new helpers. Renamed since IBool is not a thing anymore and updated docs.

---
+++ ItemChanger.Core/Events/Args/PlacementVisitedEventArgs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ItemChanger.Enums;
+using ItemChanger.Placements;
+
+namespace ItemChanger.Events.Args;
+
+/// <summary>
+/// Event arguments describing a visit to a placement
+/// </summary>
+public class PlacementVisitedEventArgs(Placement placement, VisitState proposedNewFlags) : EventArgs
+{
+    /// <summary>
+    /// Placement whose state changed.
+    /// </summary>
+    public Placement Placement => placement;
+
+    /// <summary>
+    /// Flags that were set before the change.
+    /// </summary>
+    public VisitState Orig => placement.Visited;
+
+    /// <summary>
+    /// Flags being applied in this change.
+    /// </summary>
+    public VisitState ProposedNewFlags => proposedNewFlags;
+
+    /// <summary>
+    /// The final flags expected after the change is applied.
+    /// </summary>
+    public VisitState ModifiedFlags => Orig | ProposedNewFlags;
+
+    /// <summary>
+    /// Whether any change has occurred to the visit state
+    /// </summary>
+    public bool HasChangedAny => ModifiedFlags != Orig;
+
+    /// <summary>
+    /// Whether any change has occurred to the specified flags of the visit state
+    /// </summary>
+    /// <param name="checkFlags">The flags to check for changes</param>
+    public bool HasChangedFlags(VisitState checkFlags)
+    {
+        VisitState maskedOrig = Orig & checkFlags;
+        VisitState maskedModified = ModifiedFlags & checkFlags;
+        return maskedOrig != maskedModified;
+    }
+}
